6cb1967b0298a30982338aef4f32b3e2dff16c72
FloLeh <florianlehuede@gmail.com>
2025-10-30 15:10:24 +0100

    service-report : refactoring

--- service-report/src/main/java/com/openclassrooms/servicereport/services/ReportService.java
+++ service-report/src/main/java/com/openclassrooms/servicereport/services/ReportService.java
@@ -2,13 +2,10 @@ package com.openclassrooms.servicereport.services;
 
 import com.openclassrooms.servicereport.DTOs.PatientReportResponse;
 import com.openclassrooms.servicereport.DTOs.PatientWithNotes;
-import com.openclassrooms.servicereport.enums.RiskLevel;
 
 import java.util.List;
 
 public interface ReportService {
     List<PatientWithNotes> getAllPatientsWithNotes();
-    long triggerWordsCountByPatient(PatientWithNotes patient);
-    RiskLevel calculateRiskLevel(PatientWithNotes patient);
     List<PatientReportResponse> getAllPatientReports();
 }
--- service-report/src/main/java/com/openclassrooms/servicereport/services/ReportServiceImpl.java
+++ service-report/src/main/java/com/openclassrooms/servicereport/services/ReportServiceImpl.java
@@ -7,12 +7,12 @@ import com.openclassrooms.servicereport.DTOs.PatientWithNotes;
 import com.openclassrooms.servicereport.clients.MedicalNoteClient;
 import com.openclassrooms.servicereport.clients.PatientClient;
 import com.openclassrooms.servicereport.enums.RiskLevel;
-import com.openclassrooms.servicereport.enums.TriggerWords;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.stream.Collectors.*;
 
@@ -22,7 +22,24 @@ public class ReportServiceImpl implements ReportService {
 
     private final PatientClient patientClient;
     private final MedicalNoteClient medicalNoteClient;
+    private static final int ageLimit = 30;
 
+    private final static Set<String> triggerWords = Set.of(
+            "hémoglobine a1c",
+            "microalbumine",
+            "taille",
+            "poids",
+            "fumeur",
+            "fumeuse",
+            "anormal",
+            "cholestérol",
+            "vertige",
+            "réaction",
+            "rechute",
+            "anticorps"
+    );
+
+    @Override
     public List<PatientWithNotes> getAllPatientsWithNotes() {
         List<Patient> patients = List.copyOf(patientClient.getPatientsList());
         List<MedicalNote> notes = List.copyOf(medicalNoteClient.getNoteList());
@@ -44,38 +61,7 @@ public class ReportServiceImpl implements ReportService {
                 .toList();
     }
 
-    public long triggerWordsCountByPatient(PatientWithNotes patient) {
-        return TriggerWords.countTriggers(String.join(" ", patient.notes()));
-    }
-
-    public RiskLevel calculateRiskLevel(PatientWithNotes patient) {
-        long triggerCount = triggerWordsCountByPatient(patient);
-        int age = patient.getAge();
-        String gender = patient.gender();
-
-        if (triggerCount == 0) {
-            return RiskLevel.NONE;
-        }
-
-        if (age > 30 && triggerCount >= 2 && triggerCount <= 5) {
-            return RiskLevel.BORDERLINE;
-        }
-
-        if ((age < 30 && gender.equals("M") && triggerCount >= 3 && triggerCount < 5)
-                || (age < 30 && gender.equals("F") && triggerCount >= 4 && triggerCount < 7)
-                || (age > 30 && triggerCount >= 6 && triggerCount <= 7)) {
-            return RiskLevel.IN_DANGER;
-        }
-
-        if ((age < 30 && gender.equals("M") && triggerCount >= 5)
-                || (age < 30 && gender.equals("F") && triggerCount >= 7)
-                || (age > 30 && triggerCount >= 8)) {
-            return RiskLevel.EARLY_ONSET;
-        }
-
-        return RiskLevel.NONE;
-    }
-
+    @Override
     public List<PatientReportResponse> getAllPatientReports() {
         List<PatientWithNotes> patients = getAllPatientsWithNotes();
 
@@ -85,7 +71,65 @@ public class ReportServiceImpl implements ReportService {
                     return new PatientReportResponse(
                             patient.id(),
                             riskLevel.code()
-                            );
+                    );
                 }).toList();
+    }
+
+    private static long countTriggers(final String note) {
+        return triggerWords.stream()
+                .filter(trigger -> note.toLowerCase().contains(trigger))
+                .count();
+    }
+
+
+    private long triggerWordsCountByPatient(final PatientWithNotes patient) {
+        return countTriggers(String.join(" ", patient.notes()));
+    }
+
+    private RiskLevel calculateRiskLevel(final PatientWithNotes patient) {
+        final long triggerCount = triggerWordsCountByPatient(patient);
+        final int age = patient.getAge();
+        final String gender = patient.gender();
+
+        if (isBorderline(age, triggerCount)) {
+            return RiskLevel.BORDERLINE;
+        }
+
+        if (isInDanger(age, gender, triggerCount)) {
+            return RiskLevel.IN_DANGER;
+        }
+
+        if (isEarlyOnset(age, gender, triggerCount)) {
+            return RiskLevel.EARLY_ONSET;
+        }
+
+        return RiskLevel.NONE;
+    }
+
+    private static boolean isEarlyOnset(int age, String gender, long triggerCount) {
+        return (age <= ageLimit && gender.equals("M")
+                    && triggerCount >= 5)
+                || (age <= ageLimit && gender.equals("F")
+                    && triggerCount >= 7)
+                || (age > ageLimit
+                    && triggerCount >= 8);
+    }
+
+    private static boolean isInDanger(int age, String gender, long triggerCount) {
+        return (age <= ageLimit && gender.equals("M")
+                    && triggerCount >= 3
+                    && triggerCount < 5)
+                || (age <= ageLimit && gender.equals("F")
+                    && triggerCount >= 4
+                    && triggerCount < 7)
+                || (age > ageLimit
+                    && triggerCount >= 6
+                    && triggerCount < 8);
+    }
+
+    private static boolean isBorderline(int age, long triggerCount) {
+        return age > ageLimit
+                && triggerCount >= 2
+                && triggerCount <= 5;
     }
 }
